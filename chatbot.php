26b82d01fa0df88d3e864c394028126ba4198205
Frederik Graakjær <graakjaer.f@gmail.com>
2023-09-12 15:36:08 +0200

    deleted/earlier chats stored in array

--- chatbot.php
+++ chatbot.php
@@ -2,13 +2,9 @@
 
 session_start();
 
-// Define predefined responses and questions as an associative array
-$responses = array(
-    "Hello" => "Hello! 👋",
-    "Hi" => "Hello! 👋",
-    "Hej" => "Hello! 👋",
-    "2" => "This is response 2."
-);
+// Read the JSON file into an associative array
+$responsesJSON = file_get_contents('responses.json');
+$responses = json_decode($responsesJSON, true);
 
 // Default response when the bot doesn't understand the question
 $defaultResponse = "I'm sorry, I don't understand your question.";
@@ -16,11 +12,22 @@ $defaultResponse = "I'm sorry, I don't understand your question.";
 // Get the user's input
 $userInput = isset($_POST['user_input']) ? $_POST['user_input'] : null;
 
-// Check if the user input matches a predefined response
-if (isset($responses[$userInput])) {
-    $botResponse = $responses[$userInput];
-} else{
-    $botResponse = $defaultResponse;
+// Initialize bot response as default
+$botResponse = $defaultResponse;
+
+// Check if the user input contains a word that matches a predefined response
+if (!empty($userInput)) {
+    foreach ($responses as $keyword => $response) {
+        if (stripos($userInput, $keyword) !== false) {
+            $botResponse = $response;
+            break; // Stop checking if a match is found
+        }
+    }
+}
+
+// Create a new session variable for storing deleted chats
+if (!isset($_SESSION["deletedChats"])) {
+    $_SESSION["deletedChats"] = [];
 }
 
 // Combine the bot's response with the responseData array
@@ -29,16 +36,41 @@ $_SESSION["responseData"][] = array(
     'response' => $botResponse
 );
 
+// Check if the user wants to destroy the session
+if (isset($_POST['destroy-session-button'])) {
+    // Move the current chat session to the deletedChats array
+    $_SESSION["deletedChats"][] = $_SESSION["responseData"];
+    // Clear the responseData array
+    $_SESSION["responseData"] = array();
+
+    // Destroy the session
+    //session_destroy();
+    
+    // Redirect the user back to index.php
+    header('Location: index.php');
+    exit;
+}
+
+// Replace responseData with deletedChats
+if (isset($_POST['restore-session-button'])) {
+    // Move the current chat session to the deletedChats array
+    ($_SESSION["responseData"] = $_SESSION["deletedChats"][0]);
+    // Redirect the user back to index.php
+    header('Location: index.php');
+    exit;
+}
+
+
 // Redirect the user back to index.php
 header('Location: index.php');
 exit;
 
-//Destroy the session when the user clicks the button
-if (isset($_POST['destroy-session-button'])) {
-    session_destroy();
-    header('Location: index.php'); // Redirect the user back to index.php
-    exit;
-}
+// //Destroy the session when the user clicks the button
+// if (isset($_POST['destroy-session-button'])) {
+//     session_destroy();
+//     header('Location: index.php'); // Redirect the user back to index.php
+//     exit;
+// }
 
 ?>
 
