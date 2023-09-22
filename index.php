2d0efbc6e8f044a582448d5bd2f911c6e0e42c1c
Frederik Graakjær <graakjaer.f@gmail.com>
2023-09-22 10:01:43 +0200

    old php deleted, now getting messages from fetch

--- index.php
+++ index.php
@@ -1,55 +1,32 @@
-<?php session_start(); ?>
-<!-- <?php include("test.php") 
-?> -->
-
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <title>Chatbot</title>
-</head>
-
-<body>
-    <form method="post" action="chatbot.php">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title>Chatbot</title>
+    </head>
+    
+    <body>
+    <form id="chatForm">
         <div class="chatBotWrapper">
             <div class="chatbot">
                 <div class="header">
                     <h1>Chatbot</h1>
+                    <button id="clearSessionButton" type="button">Clear session</button>
                     <img src="chatbot.png" alt="chatbot" class="chatbotImage" />
                 </div>
                     <div class="defaultRobotTextWrapper">
                         <div class="imageCircle">
                             <img src="chatbot.png" alt="chatbotBubble" class="chatbotBubbleImage" />
                         </div>
-                        <div for="user_input" class="robotText">
+                        <div for="user_input" class="defaultRobotText">
                             <p>Hello! 👋 What can i help you with today?</p>
                         </div>
                     </div>
-                 <?php if(isset ($_SESSION["responseData"])){
-                        foreach ($_SESSION["responseData"] as $chat => $chatEntry) {
-                        echo 
-                        "<div class=robotTextWrapper>
-                            <div class=flexEnd>
-                                <div class=userText>
-                                    <p>{$chatEntry['question']}</p>
-                                </div> 
-                            </div>
-                            <div class=flexStart>
-                                <div class=imageCircle>
-                                    <img src=chatbot.png alt=chatbotBubble class=chatbotBubbleImage />
-                                </div>  
-                                <div class=robotText>
-                                    <p>{$chatEntry['response']}</p>
-                                </div>
-                            </div>
-                        </div>";
-                        }}
-
-                       ?>
-                <div id="bot_response"></div>
+                <div class="robotTextWrapper" id="bot_response">
+                </div>
                     <div class="inputWrapper">
                         <div class="inputfield">
                             <input 
@@ -67,49 +44,21 @@
                                 type="submit" 
                                 value="Submit" 
                                 id="sendButton" 
-                                disabled>
+                                disabled
+                                >
                                 <img src="sendDefault.png" alt="send" class="sendImage" id="sendImg" />
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-            <?php
-            
-            // Check if deletedChats array exists and is not empty
-            echo '<div class="earlierChatsWrapper">';
-            echo '<h2>Earlier Chats</h2>';
-            if(isset($_SESSION["deletedChats"])){
-                $deletedChatsLength = count($_SESSION["deletedChats"]);
-                echo "Total chats: " . $deletedChatsLength;
-            } else{
-                echo "Total chats: 0";
-            }
-            if (isset($_SESSION["deletedChats"])) {
-                echo '<div class="">';
-                echo '<button type="submit" name="destroy-session-button" class="newChatButton">+ New chat</button>';
-                echo "<br>";
-                var_dump($_SESSION["deletedChats"][0]);
-                // Loop through deletedChats array and display deleted chats
-                foreach ($_SESSION["deletedChats"] as $chatIndex => $chat) {
-                    echo "<p> Chat ";
-                    echo $chatIndex + 1;
-                    echo "</p>";
-                    echo '<button type="submit" name="restore-session-button">Restore chat</button>';
-                    echo '</>'; 
-                }
-                echo '</div>'; 
-            }
-            echo "</div>";
-            ?>
-         
         </form>
+    </body>
+    <script src="script.js"></script> 
+    <script src="clearSession.js"></script> 
 
-        <script>
-        // While loop to simulate a delay
-        // let startTime = new Date().getTime();
-        // while (new Date().getTime() < startTime + 3000);
-
+    <script></script>
+ <script>
         function characterCounter(){
             var text = document.getElementById("user_input").value;
             var charCount = text.length;
@@ -128,8 +77,6 @@
             var element = document.getElementById("bot_response");
             element.scrollTop = element.scrollHeight;
         }
-
-
     </script>
 </body>
 
