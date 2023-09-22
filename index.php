0b4ec74b473521e1e8c711c009603a57dd4d0c79
Frederik Graakjær <graakjaer.f@gmail.com>
2023-09-22 13:17:34 +0200

    deleted charactercounter from index.php

--- index.php
+++ index.php
@@ -35,10 +35,10 @@
                             name="user_input" 
                             id="user_input" 
                             placeholder="Enter a message" 
-                            oninput="characterCounter()" 
+                            oninput="characterCounter()"
                             maxlength="500" />
                             <p>
-                                <span id="charCount" value="characterCounter()">0</span>/500</p>
+                                <span id="charCount">0</span>/500</p>
                                 <button 
                                 class="sendButton" 
                                 type="submit" 
@@ -53,31 +53,8 @@
                 </div>
             </div>
         </form>
-    </body>
     <script src="script.js"></script> 
     <script src="clearSession.js"></script> 
-
-    <script></script>
- <script>
-        function characterCounter(){
-            var text = document.getElementById("user_input").value;
-            var charCount = text.length;
-            document.getElementById("charCount").innerHTML = charCount;
-            
-             if(text.length <= 0){
-                document.getElementById("sendButton").disabled = true;
-                document.getElementById("sendImg").src="sendDefault.png";
-            } else{
-                document.getElementById("sendButton").disabled = false;
-                document.getElementById("sendImg").src="send.png";
-            }
-        }
-
-        function scrollToBottom(){
-            var element = document.getElementById("bot_response");
-            element.scrollTop = element.scrollHeight;
-        }
-    </script>
-</body>
+    </body>
 
 </html>
